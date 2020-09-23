8b555faa848ab99f51f89c7eeaac9892bbded808
bradley1492 <bradleytochristopher@gmail.com>
2020-09-23 18:40:28 +0200

    Complete initial phaser-3 setup

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
 
-const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
+var game = new Phaser.Game(800,600,Phaser.AUTO,'',{preload : preload, create : create, update : update,});
 
-const game = new Phaser.Game(config);
+function preload(){
 
-function preload() {
-  this.load.image("logo", logoImg);
 }
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
+function create(){
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+}
+
+function update(){
+  
 }
